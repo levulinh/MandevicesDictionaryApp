bd5458d25786fe5d799dd04b7603d928f728bb1e
Linh Le Vu <levulinhchv@gmail.com>
2018-07-14 11:17:27 +0700

    Tiếp tục về splash Screen

--- app/src/main/java/com/andrew/studio/mandevicesdictionaryapp/MainActivity.kt
+++ app/src/main/java/com/andrew/studio/mandevicesdictionaryapp/MainActivity.kt
@@ -5,6 +5,11 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
 
+    //TODO 1: Sửa Launcher Activity thành Splash Screen
+        //Vào file Manifest
+    //TODO 2: Tạo style mới loại bỏ title bar
+    //TODO 3: Apply style mới này cho splash screen
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
